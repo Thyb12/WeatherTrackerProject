ad46ebaab76a0b6191258e79dde0add9d3f58e7d
Thibaut <jobardthibaut12@gmail.com>
2022-10-13 17:37:13 +0200

    no warning in code

--- app/src/main/java/com/example/weathertrackerproject/model/Current.kt
+++ app/src/main/java/com/example/weathertrackerproject/model/Current.kt
@@ -36,4 +36,51 @@ data class Current(
     val precip get() = _precip
     val humidity get() = _humidity
     val cloudCover get() = _cloudCover
+
+
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Current
+
+        if (_temperature != other._temperature) return false
+        if (!_weather_icons.contentEquals(other._weather_icons)) return false
+        if (!_weather_descriptions.contentEquals(other._weather_descriptions)) return false
+        if (_wind_speed != other._wind_speed) return false
+        if (_wind_degree != other._wind_degree) return false
+        if (_wind_dir != other._wind_dir) return false
+        if (_pressure != other._pressure) return false
+        if (_precip != other._precip) return false
+        if (_humidity != other._humidity) return false
+        if (_cloudCover != other._cloudCover) return false
+        if (temperature != other.temperature) return false
+        if (!weatherIcons.contentEquals(other.weatherIcons)) return false
+        if (!weatherDescriptions.contentEquals(other.weatherDescriptions)) return false
+        if (windSpeed != other.windSpeed) return false
+        if (windDegree != other.windDegree) return false
+        if (windDir != other.windDir) return false
+        if (pressure != other.pressure) return false
+        if (precip != other.precip) return false
+        if (humidity != other.humidity) return false
+        if (cloudCover != other.cloudCover) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = _temperature
+        result = 31 * result + _weather_icons.contentHashCode()
+        result = 31 * result + _weather_descriptions.contentHashCode()
+        result = 31 * result + _wind_speed
+        result = 31 * result + _wind_degree
+        result = 31 * result + _wind_dir.hashCode()
+        result = 31 * result + _pressure
+        result = 31 * result + _precip
+        result = 31 * result + _humidity
+        result = 31 * result + _cloudCover
+        return result
+    }
 }
